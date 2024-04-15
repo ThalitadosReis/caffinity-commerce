d40addd2b39a5128064674dd8213e84ba8123b40
Thalita dos Reis <dosreistha@gmail.com>
2024-04-15 18:40:21 +0200

    products list into a db

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import AllProducts from "./pages/AllProducts";
+import Wishlist from "./pages/Wishlist";
+import productsData from "../db.json";
 
-import img1 from "./assets/img1.png";
+const products = productsData;
 
 export default function App() {
-  // Define the products list
   const [likedProducts, setLikedProducts] = useState([]);
 
   const handleLikeClick = (productId) => {
@@ -22,39 +23,13 @@ export default function App() {
 
   const isProductLiked = (productId) => likedProducts.includes(productId);
 
-  const products = [
-    {
-      id: 1,
-      imgSrc: img1,
-      productName: "Product 1",
-      price: 35,
-    },
-    {
-      id: 2,
-      imgSrc: img1,
-      productName: "Product 2",
-      price: 40,
-    },
-    {
-      id: 3,
-      imgSrc: img1,
-      productName: "Product 3",
-      price: 42,
-    },
-    {
-      id: 4,
-      imgSrc: img1,
-      productName: "Product 4",
-      price: 37,
-    },
-  ];
-
   return (
     <>
       <Navbar />
 
       <Routes>
         <Route exact path="/all-products" element={<AllProducts products={products} handleLikeClick={handleLikeClick} isProductLiked={isProductLiked} />} />
+        <Route exact path="/wishlist" element={<Wishlist likedProducts={likedProducts} setLikedProducts={setLikedProducts} />} />
         <Route exact path="/" element={<Homepage />} />
       </Routes>
     </>
